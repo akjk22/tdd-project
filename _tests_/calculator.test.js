d9301748abbb86e401870966f1fc894e6a5c41ba
Andrew Kim <andrewswkim47@gmail.com>
2020-03-05 20:44:26 -0800

    test for earth age now passing. Green light.

--- _tests_/calculator.test.js
+++ _tests_/calculator.test.js
@@ -3,6 +3,6 @@ import { Calculator } from './../src/calculator';
 describe ('Calculator', () => {
     test('that earth age will be calculated by multiplying inputted age in years by 365', () => {
         let calculator = new Calculator();
-        expect(calculator.earthAge(20)).toEqual(7500);
+        expect(calculator.earthAge(20)).toEqual(7300);
     });
 });
