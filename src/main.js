73b083652aa33a1dc81a8d9d7a23ca14e61ade40
Andrew Kim <andrewswkim47@gmail.com>
2020-03-06 17:40:23 -0800

    Add interface logic

--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@ import { Mars } from './mars.js';
 import { Mercury } from './mercury.js';
 import { Jupiter } from './jupiter.js';
 import { Venus } from './venus.js';
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
 
 let calculator = new Calculator(20);
 console.log(calculator.earthAge(20));
+
+$(document).ready(function() {
+  $('#form').submit(function(event) {
+    
+  });
+});
