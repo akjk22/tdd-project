e69acac0441e1f66e7f44d122c85c47bb3d857a6
Andrew Kim <andrewswkim47@gmail.com>
2020-03-07 14:27:03 -0800

    work on getting interface to work

--- src/main.js
+++ src/main.js
@@ -1,21 +1,27 @@
-import { Calculator } from './calculator.js';
 import { Mars } from './mars.js';
 import { Mercury } from './mercury.js';
 import { Jupiter } from './jupiter.js';
 import { Venus } from './venus.js';
-import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-
-
-let calculator = new Calculator(20);
-console.log(calculator.earthAge(20));
-
 $(document).ready(function() {
-  $('#form').submit(function(event) {
-    
+  $('form#age').submit(function(event) {
+    event.preventDefault();
+    let age = parseInt($("input#age").val());
+    let mars = new Mars(age);
+    let jupiter = new Jupiter(age);
+    let mercury = new Mercury(age);
+    let venus = new Venus(age);
+
+    mars.marsAge(age);
+    mars.lifeExpectancy(age);
+    jupiter.lifeExpectancy(age);
+    mercury.lifeExpectancy(age);
+    venus.lifeExpectancy(age);
+    console.log(mars.lifeExpectancy(age));
+    $("#result").append("<p>"+ mars.marsAge(age) + "<br>" + mars.lifeExpectancy(age) + "<p>");
   });
 });
