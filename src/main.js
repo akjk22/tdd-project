1b18875a3bab6481ff162840cd252cc6157a4032
Andrew Kim <andrewswkim47@gmail.com>
2020-03-07 19:00:57 -0800

    submit all passed tests and partially working interface

--- src/main.js
+++ src/main.js
@@ -11,17 +11,24 @@ $(document).ready(function() {
   $('form#age').submit(function(event) {
     event.preventDefault();
     let age = parseInt($("input#age").val());
-    let mars = new Mars(age);
-    let jupiter = new Jupiter(age);
-    let mercury = new Mercury(age);
-    let venus = new Venus(age);
-
-    mars.marsAge(age);
-    mars.lifeExpectancy(age);
-    jupiter.lifeExpectancy(age);
-    mercury.lifeExpectancy(age);
-    venus.lifeExpectancy(age);
-    console.log(mars.lifeExpectancy(age));
-    $("#result").append("<p>"+ mars.marsAge(age) + "<br>" + mars.lifeExpectancy(age) + "<p>");
+    let planet = $("input#planet").val();
+    // let mars = $("input:name='mars']:checked").val();
+    let mars = new Mars(age, planet);
+    // let jupiter = new Jupiter(age);
+    // let mercury = new Mercury(age);
+    // let venus = new Venus(age);
+    alert(planet);
+    
+    // let planetMars = new Mars(age, mars);
+  
+ 
+    // mars.lifeExpectancy(age);
+    
+    // jupiter.lifeExpectancy(age);
+    // mercury.lifeExpectancy(age);
+    // venus.lifeExpectancy(age);
+   
+    $("#result").text(" You are " + mars.marsAge(age) + " on planet " + mars.planetName() + " and your life expectancy there is" + " " + mars.lifeExpectancy(age) + " and ");
+    // $("#result").append("<p>"+ mars.marsAge(age) + "<br>" + mars.lifeExpectancy(age) + "<p>");
   });
 });
--- src/mars.js
+++ src/mars.js
@@ -1,6 +1,7 @@
 export class Mars {
   constructor(age) {
     this.age = age;
+    this.name = "Mars";
   }
   marsAge(age) {
     this.age = age * 365 / (365 * 1.88);
@@ -12,5 +13,10 @@ export class Mars {
       return this.marsAge(age) - lifeExpectancy;
     }
     return lifeExpectancy - this.marsAge(age);
+  }
+  planetName() {
+      if (this.age = age * 365 / (365 * 1.88)); {
+      return this.name;
+      }
   }
 }
